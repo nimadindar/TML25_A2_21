338ac40860046a8cfb0b35e7509f5fad984ba295
nimadindar <nima.dindarsafa@gmail.com>
2025-06-16 15:04:25 +0200

    updated config and dataset to add augmentations

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,31 +1,35 @@
 import torch
 from torch.utils.data import Dataset
 
+from PIL import Image
 from typing import Tuple, List
+
+from config import Augmentations
 
 
 class TaskDataset(Dataset):
 
     def __init__(
             self,
-            api_embeddings: torch.Tensor,
-            base_transform,
-            num_augs: int = 4,):
+            api_embeddings: torch.Tensor,):
         
         self.ids = []       
         self.imgs = []     
         self.labels = []         
 
         self.api_embeddings = api_embeddings
-        self.base_transform = base_transform
-        self.num_augs = num_augs
+        self.transforms = Augmentations.AUGMENTATION_SET
 
     def __len__(self) -> int:
         return len(self.imgs)
     
     def __getitem__(self, index) -> List[torch.Tensor]:
         img = self.imgs[index]
-        views = [self.base_transform(img) for _ in range(self.num_augs)]
+        if isinstance(img, str):
+            img = Image.open(img).convert("RGB")
+        else:
+            img = img.convert("RGB")
+        views = [t(img) for t in self.transforms]
         target = self.api_embeddings[index]        
         return img, views, target
         
