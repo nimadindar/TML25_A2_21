d5a4e116534d47ab027cf395841761106de4b61a
nimadindar <nima.dindarsafa@gmail.com>
2025-06-17 00:59:37 +0200

    cleaned main.py for QUERY

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
+import os
+import sys
+
 np.random.seed(TrainingConfig.SEED)
 torch.cuda.manual_seed_all(TrainingConfig.SEED)
 
@@ -29,17 +32,23 @@ if REQUEST_NEW_API:
 
 
 elif QUERY:
-    dataset = torch.load("./data/ModelStealingPub.pt", weights_only=False)
-    subset = get_random_subset(dataset, subset_index=APIConfig.IDX, seed= TrainingConfig.SEED)
-    model_stealer = ModelStealer(APIConfig.TOKEN)
-
-    image_ids = [dataset.ids[i] for i in subset.indices]
-    images = [dataset.imgs[i] for i in subset.indices]
-
-    representations = model_stealer.query_api(images, image_ids, APIConfig.IDX, APIConfig.PORT)
+    if os.path.exists(f"./results/out{APIConfig.IDX}"):
+        print(f"Representation file already exists for the given subset ID: {APIConfig.IDX}. \
+              If you have obtained a new port/seed remove the existing file and try again.")
+        sys.exit(0)
     
-    print(f"The representation for the given subset with id {APIConfig.IDX} with length {len(representations)} has been fetched. \
-            \n To view th output please refer to file saved in ./results/out{APIConfig.IDX}.pickle")
+    else:
+        dataset = torch.load("./data/ModelStealingPub.pt", weights_only=False)
+        subset = get_random_subset(dataset, subset_index=APIConfig.IDX, seed= TrainingConfig.SEED)
+        model_stealer = ModelStealer(APIConfig.TOKEN)
+
+        image_ids = [dataset.ids[i] for i in subset.indices]
+        images = [dataset.imgs[i] for i in subset.indices]
+
+        representations = model_stealer.query_api(images, image_ids, APIConfig.IDX, APIConfig.PORT)
+
+        print(f"The representation for the given subset with id {APIConfig.IDX} with length {len(representations)} has been fetched. \
+                \n To view th output please refer to file saved in ./results/out{APIConfig.IDX}.pickle")
 
 
 elif STEAL:
